ec41ef66f8448a910a4919935c545e15ba793c35
Mazurek-K <mazurekk21@gmail.com>
2024-12-12 11:29:49 +0100

    zad 1,2

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -4,5 +4,39 @@
 // tutaj definicja klasy Makaron
 // BEZ definicji metody gotujMakaron (tylko z deklaracją)
 
+
+class Makaron
+{
+public:
+    virtual double ileMaki(unsigned P) const = 0;
+    static Makaron* gotujMakaron(std::string input);
+
+    //virtual ~Makaron() = default;
+};
+
+
+
 // Zad1
 // tutaj definicja klasy Tagliatelle
+class Tagliatelle : public Makaron
+{
+private:
+    double L;
+    double W;
+    double R;
+    static const double C;
+
+public:
+    Tagliatelle(double l, double w, double r) : L(l), W(w), R(r) {}
+    //Tagliatelle() : L{0.5}, W{0.5}, R{0.5} {}
+    Tagliatelle() : L(0.5), W(0.5), R(0.5) {}
+
+    double ileMaki(unsigned P) const  {
+
+        return P * L * W * (1. - R) * C;
+    }
+
+   
+
+};
+
--- include/zad2.hpp
+++ include/zad2.hpp
@@ -7,3 +7,27 @@
 // tutaj klasa Penne
 
 // tutaj definicja metody gotujMakaron
+
+
+
+class Penne : public Makaron
+{
+
+public:
+    Penne(){};
+    double ileMaki(unsigned P) const override { return static_cast< double >(P); }
+};
+
+
+Makaron* Makaron::gotujMakaron(std::string input)
+{
+    if (input.front() == input.back())
+    {
+        return new Tagliatelle(3.14, 0.42, 0.1);
+    }
+    else
+    {
+        return new Penne();
+    }
+}
+
