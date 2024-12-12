86b5529040107ce92aaf07548d01aafdd9d9e9f4
Mazurek-K <mazurekk21@gmail.com>
2024-12-12 12:09:23 +0100

    w zadn 4 nie przesyłam przez referencje a działa, nie rozmiem czegoś? czy bład w zadaniu

--- include/zad4.hpp
+++ include/zad4.hpp
@@ -3,3 +3,26 @@
 #include <vector>
 
 // tutaj funkcja obliczMake
+double obliczMake(std::vector<Tagliatelle> vec)
+{
+    double M = 0;
+    int    i = 1;
+    for (auto it = vec.rbegin(); it != vec.rend(); ++it)
+    {
+        M=M+it->ileMaki(i);
+        ++i;
+    }
+
+    if (M > 100) {
+        throw 40;
+    }
+    else if (M>50 && M<=100)
+    {
+        throw 6.9;
+    }
+    else
+    {
+        return M;
+    }
+
+}
